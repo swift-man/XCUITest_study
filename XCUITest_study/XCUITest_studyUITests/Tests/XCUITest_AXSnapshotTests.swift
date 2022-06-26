10ac51e737785d67affc1ec389ea7cdf1f9b6dc8
jiniopening <jiniopening@naver.com>
2022-06-27 00:59:00 +0900

    [Test] add testing XCUITest_AXSnapshotTests

---
+++ XCUITest_study/XCUITest_studyUITests/Tests/XCUITest_AXSnapshotTests.swift
@@ -0,0 +1,43 @@
+//
+//  XCUITest_AXSnapshotTests.swift
+//  XCUITest_studyUITests
+//
+//  Created by SwiftMan on 2022/06/27.
+//
+
+import XCTest
+import Reusable
+import AXSnapshot
+@testable import XCUITest_study
+
+class XCUITest_AXSnapshotTests: XCTestCase {
+  func testViewControllerAXSnapshot() {
+    let vc = ViewController.instantiate()
+    
+    let result = """
+      ------------------------------------------------------------
+      Enter Your City
+      staticText
+      ------------------------------------------------------------
+      cityTextField
+      TextField
+      ------------------------------------------------------------
+      Enroll
+      button
+      ------------------------------------------------------------
+      TAUlogo
+      image
+      ------------------------------------------------------------
+      Welcome to XCUITest Course
+      staticText
+      ------------------------------------------------------------
+      testView.Label.text
+      staticText
+      ------------------------------------------------------------
+      """
+    
+    XCTAssert(
+      vc.axSnapshot() == result
+    )
+  }
+}
