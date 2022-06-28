61e8c1b598440e037b96fdddcee8924fa7b0812a
jiniopening <jiniopening@naver.com>
2022-06-28 15:53:00 +0900

    [Feat] AssetSnapshotTests 제거

--- XCUITest_study/XCUITest_studyUITests/Tests/XCUITest_AssetSnapshotTests.swift
+++ XCUITest_study/XCUITest_studyUITests/Tests/XCUITest_AssetSnapshotTests.swift
@@ -11,21 +11,21 @@ import Reusable
 @testable import XCUITest_study
 
 class XCUITest_AssetSnapshotTests: XCTestCase {
-  func testViewControllerAssertSnapshot() {
-    let vc = ViewController.instantiate()
-    
-    assertSnapshot(matching: vc, as: .image)
-  }
-  
-  func testTestViewAssertSnapshot() {
-    let data = "testScreenshot"
-    let view = TestView(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-    view.text = data
-
-    view.layoutIfNeeded()
-    let screenshot = view.asImage()
-    assertSnapshot(matching: screenshot,
-                   as: .image,
-                   named: "TestView")
-  }
+//  func testViewControllerAssertSnapshot() {
+//    let vc = ViewController.instantiate()
+//
+//    assertSnapshot(matching: vc, as: .image)
+//  }
+//  
+//  func testTestViewAssertSnapshot() {
+//    let data = "testScreenshot"
+//    let view = TestView(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+//    view.text = data
+//
+//    view.layoutIfNeeded()
+//    let screenshot = view.asImage()
+//    assertSnapshot(matching: screenshot,
+//                   as: .image,
+//                   named: "TestView")
+//  }
 }
